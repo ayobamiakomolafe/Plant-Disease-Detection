1ba41712da7a82d22c5a78b20355fd7a8a42cfcd
Ayobami <68803580+ayobamiakomolafe@users.noreply.github.com>
2025-03-10 21:10:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ def classification(path):
 		  'Raspberry___healthy','Soybean___healthy','Squash___Powdery_mildew','Strawberry___healthy','Strawberry___Leaf_scorch']
 
     model.load_weights("rps.h5") #load pretrained model
-    img = image.load_img(path,target_size=(150,150))  
-    x = image.img_to_array(img)
+    img = load_img(path,target_size=(150,150))  
+    x = img_to_array(img)
     x = np.expand_dims(x , axis=0)
     images = np.vstack([x])
     classes = model.predict(images , batch_size =10) # predicting images
